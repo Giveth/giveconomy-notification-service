6c7609d6f8a845c69b1a531cdf2223726fdc5ca5
Amin Latifi <a.latifi.al@gmail.com>
2023-02-20 14:47:44 +0330

    Changed production start block

--- monitorConfig/production.ts
+++ monitorConfig/production.ts
@@ -18,13 +18,13 @@ export const config: Config = {
 				{
 					address: '0xD93d3bDBa18ebcB3317a57119ea44ed2Cf41C2F2',
 					title: 'GIVPower',
-					startBlock: 24461201,
+					startBlock: 26572747,
 					type: ContractType.GIVpower,
 				},
 				{
 					address: '0xc0dbDcA66a0636236fAbe1B3C16B1bD4C84bB1E1',
 					title: 'Gnosis Token Distro',
-					startBlock: 19731573,
+					startBlock: 26572747,
 					type: ContractType.TokenDistro,
 				},
 			],
